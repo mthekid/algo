8286f9d8cfe4af71942dc02b7fbadd6494a5b5a7
= <=>
2021-11-29 16:13:40 +0900

    반대 방향과 관련된 포인터 문제. right, left 변수명!

--- src/com/company/algomonster/twopointer/TwoSumSorted.java
+++ src/com/company/algomonster/twopointer/TwoSumSorted.java
@@ -1,9 +1,14 @@
 package com.company.algomonster.twopointer;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * 투 포인터 기법중 양 끝에서 이동하는 기법을 활용.
+ * 시작 점과 끝 점을 활용해서 문제를 푼다.
+ */
 public class TwoSumSorted {
 
     public static void main(String[] args) {
@@ -25,7 +30,35 @@ public class TwoSumSorted {
     }
 
     private static List<Integer> twoSumSorted(List<Integer> arr, int target) {
+        /* 나의 정답.
+        int startIdx = 0;
+        int endIdx = arr.size() - 1;
+        List<Integer> ans = new LinkedList<>();
 
-        return List.of();
+        for(;;) {
+            int comp = arr.get(startIdx) + arr.get(endIdx);
+            if(comp == target) {
+                ans.add(startIdx);
+                ans.add(endIdx);
+                break;
+            } else if(comp > target) {
+                endIdx = endIdx - 1;
+            } else {
+                startIdx = startIdx + 1;
+            }
+        }
+        return ans;
+        */
+        // solution [ right, left 변수명을 둔다. ] [ 우아하다. ]
+        int l = 0;
+        int r = arr.size() - 1;
+        while(l < r) {
+            int twoSum = arr.get(l) + arr.get(r);
+            if(twoSum == target) return List.of(l, r);
+            else if(twoSum < target) l++;
+            else r--;
+        }
+
+        return null;
     }
 }

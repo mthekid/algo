bb9c3a680f02afb5ddf9e6978721f01a37b8a12e
= <=>
2021-12-08 11:28:51 +0900

    triplet을 구하는 문제이다. 3개의 정수를 더해서 0이 되는 리스트 구하기. 3개의 포인터를 사용하는 문제로 난이도가 높다.

--- src/com/company/algomonster/twopointer/oposite/ThreeSum.java
+++ src/com/company/algomonster/twopointer/oposite/ThreeSum.java
@@ -1,9 +1,6 @@
 package com.company.algomonster.twopointer.oposite;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * 3개의 정수를 더해서 0이 되는 리스트를 구해라. 해당 리스트는 정렬된 순서로 처리되어야 한다.
@@ -12,8 +9,9 @@ import java.util.List;
  */
 public class ThreeSum {
     public static void main(String[] args) {
-        List<Integer> nums = List.of(-1, 0, 1, 2, -1, -4);
+        List<Integer> nums = Arrays.asList(-1, 0, 1, 2, -1, -4);
         List<List<Integer>> res = tripletsWithSum0(nums);
+        System.out.println(res);
     }
 
     /**
@@ -29,13 +27,57 @@ public class ThreeSum {
         for(int num : nums) {
             numCount.merge(num, 1, Integer::sum);
         }
-        System.out.println(numCount);
+//        System.out.println(numCount);
 
-        // 고유한 정보들을 key로 둔다.
+        // 고유한 정보들을 key로 둔다. [ 여기까지가 전처리 과정이다. ]
         List<Integer> uniqueNums = new ArrayList<>(numCount.keySet());
         Collections.sort(uniqueNums);
-        System.out.println(uniqueNums);
+//        System.out.println(uniqueNums);
 
-        return List.of();
+        // 실제 알고리즘 처리 과정
+        /**
+         * 정렬된 고유 값들을 기반으로 triplet의 포인터를 구한다.
+         * i는 첫번쨰 pointer이고
+         * j는 두번째 포인터이다.
+         * third num 은 -i -j의 값이다. map자료구조를 사용했기에 thirdNum이 존재하는지 여부를 쉽게 조회할 수 있따.
+         */
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < uniqueNums.size(); i++) {
+            int firstNum = uniqueNums.get(i);
+
+            // -1을 처리해서 두번째 세번째 포인터에서 참조하지 못하게끔 만든다.
+            numCount.merge(firstNum, -1, Integer::sum);
+
+            for(int j = i; j < uniqueNums.size(); j++) {
+                int secondNum = uniqueNums.get(j);
+                // 세번째 Num의 경우가 존재하는지를 체크해야 한다.
+                int thirdNum = -firstNum - secondNum;
+
+                // 정렬되었기에 thirdNum이 secondNum보다 작은 경우 second 포인터 다음으로 존재하지 않음을 알 수 있다.
+                if(thirdNum < secondNum) break;
+
+                // secondNum 존재하지 않는 경우(firstNum과 중복되면서 여분이 없는 경우)
+                if(numCount.get(secondNum) <= 0) continue;
+
+                // 존재하는 경우로 firstNum, secondNum, thirdNum으로 triplet을 얻을 수 있다.
+                numCount.merge(secondNum, -1, Integer::sum);
+
+                // thirdNum이 존재하지 않는 경우 혹은 2번째 값과 동일한 경우이다.
+                // 이 경우는 thirdNum을 사용해서는 안된다. 다음으로 넘어간다.
+                if(numCount.getOrDefault(thirdNum, 0) <= 0) {
+                    numCount.merge(secondNum, 1, Integer::sum);
+                    continue;
+                }
+
+                ArrayList<Integer> newList = new ArrayList<>();
+                newList.add(firstNum);
+                newList.add(secondNum);
+                newList.add(thirdNum);
+                res.add(newList);
+                numCount.merge(secondNum, 1, Integer::sum);
+            }
+        }
+
+        return res;
     }
 }
